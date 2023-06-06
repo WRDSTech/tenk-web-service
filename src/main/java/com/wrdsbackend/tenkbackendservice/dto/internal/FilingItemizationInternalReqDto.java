8d0caa43edef7a638b636d3115ad70d7fdfa9a99
jwhuang42 <jwhuang11235@gmail.com>
2023-06-06 02:15:28 -0700

    Revert "Add 10-k web service v2 basic setup code."
    This reverts commit f3bf74002f1072b9b1ea9d1239b908939a555800.

--- src/main/java/com/wrdsbackend/tenkbackendservice/dto/internal/FilingItemizationInternalReqDto.java
+++
@@ -1,5 +0,0 @@
-package com.wrdsbackend.tenkbackendservice.dto.internal;
-
-
-public record FilingItemizationInternalReqDto(String url) {
-}
--- src/main/java/com/wrdsbackend/tenkbackendservice/error/FilingContentFetchException.java
+++
@@ -1,9 +0,0 @@
-package com.wrdsbackend.tenkbackendservice.error;
-
-import org.springframework.http.HttpStatusCode;
-
-public class FilingContentFetchException extends RuntimeException{
-    public FilingContentFetchException(String url, HttpStatusCode code) {
-        super(String.format("Failed to fetch filing content from given url: %s, status code: %d", url, code.value()));
-    }
-}
